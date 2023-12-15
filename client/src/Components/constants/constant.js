e48e6806241df1d68fe41fc4330867b52cfabb48
Moustafa8 <moutafa_natour@gmail.com>
2023-12-15 04:05:07 +0200

    fixed spell error

--- client/src/Components/constants/constant.js
+++ client/src/Components/constants/constant.js
@@ -1,2 +1,2 @@
-export const baseURL = "https://mhammadharb-server.vercel.app/api/";
+export const baseURL = "https://mhammadharb-server.vercel.app/api";
 // export const baseURL = "http://localhost:4000/api";
