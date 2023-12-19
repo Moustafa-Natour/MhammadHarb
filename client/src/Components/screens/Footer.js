e376148f419e3ba71163a203c46015ef2ac32f05
Moustafa8 <moutafa_natour@gmail.com>
2023-12-19 15:02:47 +0200

    V2.0 test in Footer

--- client/src/Components/screens/Footer.js
+++ client/src/Components/screens/Footer.js
@@ -1,10 +1,16 @@
 // Footer.js
 import React, { useContext } from 'react';
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import colors from '../constants/colors';
 import { Link } from 'react-router-dom';
 import '../constants/font.css';
 import UserContext from './UserContext';
-import { AppBar, styled, useTheme, Toolbar, ButtonGroup, Stack, IconButton, Typography } from '@mui/material';
+import { AppBar, styled, useTheme, Toolbar, ButtonGroup, Stack, IconButton, Typography, Container, Stack } from '@mui/material';
 const drawerWidth = 240;
 const MAppBar = styled(AppBar, {
     shouldForwardProp: (prop) => prop !== 'open',
@@ -34,9 +40,22 @@ function Footer() {
                 <Typography variant='body1' sx={{ fontFamily: 'PlayBold', color: colors.custPrimary }}>
                     &copy; 2023 <Link style={{ color: 'inherit', fontFamily: 'BlackOpsOne', textDecoration: 'none' }} to="/">MHAMMAD HARB</Link> All rights reserved
                 </Typography>
+                <Container maxWidth="md" sx={{ mt: 8 }}>
+                    <Stack direction="row" alignItems="center" spacing={4}>
+                        <InstagramIcon fontSize="large" sx={{ color: "#E1306C" }} />
+                        <YouTubeIcon fontSize="large" sx={{ color: "#FF0000" }} />
+                        <FacebookIcon color="primary" fontSize="large" />
+                        <TwitterIcon fontSize="large" sx={{ color: "#1DA1F2" }} />
+                        <PinterestIcon fontSize="large" sx={{ color: "#E60023" }} />
+                        <LinkedInIcon fontSize="large" color="primary" />
+                    </Stack>
+                </Container>
             </Toolbar>
         </MAppBar>
     );
 }
 
 export default Footer;
+
+
+
